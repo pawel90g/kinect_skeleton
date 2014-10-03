065716b724938b2ca38a9b0df65eec1e128e3519
Pawel Garbacik <pawel.garbacik@jazzy.pro>
2014-10-03 21:11:31 +0200

    Przeniesienie projektu na Git

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,483 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+    using System;
+    using System.Windows.Media.Imaging;
+    using System.Timers;
+    using System.Media;
+
+    public partial class MainWindow : Window
+    {
+        private const float RenderWidth = 640.0f;
+        private const float RenderHeight = 480.0f;
+        private const double JointThickness = 3;
+        private const double BodyCenterThickness = 10;
+        private const double ClipBoundsThickness = 10;
+        private readonly Brush centerPointBrush = Brushes.Blue;
+        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
+        private readonly Brush inferredJointBrush = Brushes.Yellow;
+        private readonly Pen trackedBonePen = new Pen(Brushes.Green, 6);
+        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+
+        private KinectSensor sensor;
+
+        private DrawingGroup drawingGroup;
+        private DrawingImage imageSource;
+
+        private bool[,] wall;
+        private Timer timer = null;
+        private int counter = 5;
+        private static int level = 1;
+        private bool matched = false;
+        private int points = 0;
+        private string _wall;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draws indicators to show which edges are clipping skeleton data
+        /// </summary>
+        /// <param name="skeleton">skeleton to draw clipping information for</param>
+        /// <param name="drawingContext">drawing context to draw to</param>
+        private static void RenderClippedEdges(Skeleton skeleton, DrawingContext drawingContext)
+        {
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Bottom))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, RenderHeight - ClipBoundsThickness, RenderWidth, ClipBoundsThickness));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Top))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, 0, RenderWidth, ClipBoundsThickness));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Left))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, 0, ClipBoundsThickness, RenderHeight));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Right))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(RenderWidth - ClipBoundsThickness, 0, ClipBoundsThickness, RenderHeight));
+            }
+        }
+
+        /// <summary>
+        /// Execute startup tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Create the drawing group we'll use for drawing
+            this.drawingGroup = new DrawingGroup();
+
+            // Create an image source that we can use in our image control
+            this.imageSource = new DrawingImage(this.drawingGroup);
+
+            // Display the drawing using our image control
+            Image.Source = this.imageSource;
+            this.closeBtn.Visibility = System.Windows.Visibility.Hidden;
+            this.restartBtn.Visibility = System.Windows.Visibility.Hidden;
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+
+                this.startNewLevel();
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's SkeletonFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+
+            using (DrawingContext dc = this.drawingGroup.Open())
+            {
+                // Draw a transparent background to set the render size
+                dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+
+                if (skeletons.Length != 0)
+                {
+                    foreach (Skeleton skel in skeletons)
+                    {
+                        RenderClippedEdges(skel, dc);
+
+                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            this.DrawBonesAndJoints(skel, dc);
+                            if (ifJointsCollideWithWall(skel))
+                            {
+                                dc.DrawText(new FormattedText("Źle!", System.Globalization.CultureInfo.CurrentCulture,
+                                    System.Windows.FlowDirection.LeftToRight, new Typeface("Verdena"), 24,
+                                    new SolidColorBrush(Colors.Blue)), new Point(0, 0));
+
+                                this.matched = false;
+                            }
+                            else
+                            {
+                                dc.DrawText(new FormattedText("Dobrze!", System.Globalization.CultureInfo.CurrentCulture,
+                                    System.Windows.FlowDirection.LeftToRight, new Typeface("Verdena"), 24,
+                                    new SolidColorBrush(Colors.Blue)), new Point(0, 0));
+
+                                this.matched = true;
+                            }
+                        }
+                        else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
+                        {
+                            dc.DrawEllipse(
+                            this.centerPointBrush,
+                            null,
+                            this.SkeletonPointToScreen(skel.Position),
+                            BodyCenterThickness,
+                            BodyCenterThickness);
+                        }
+                    }
+                }
+
+                // prevent drawing outside of our render area
+                this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+            }
+        }
+
+        /// <summary>
+        /// Draws a skeleton's bones and joints
+        /// </summary>
+        /// <param name="skeleton">skeleton to draw</param>
+        /// <param name="drawingContext">drawing context to draw to</param>
+        private void DrawBonesAndJoints(Skeleton skeleton, DrawingContext drawingContext)
+        {
+            drawingContext.DrawImage(new BitmapImage(new Uri(_wall, UriKind.Relative)), new Rect(0, 0, RenderWidth, RenderHeight));
+
+            // Render Torso
+            this.DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
+            this.DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
+
+            // Left Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
+
+            // Right Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
+            this.DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
+
+            // Left Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
+
+            // Right Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
+
+            // Render Joints
+            foreach (Joint joint in skeleton.Joints)
+            {
+                Brush drawBrush = null;
+
+                if (joint.TrackingState == JointTrackingState.Tracked)
+                {
+                    drawBrush = this.trackedJointBrush;
+                }
+                else if (joint.TrackingState == JointTrackingState.Inferred)
+                {
+                    drawBrush = this.inferredJointBrush;
+                }
+
+                if (drawBrush != null)
+                {
+                    drawingContext.DrawEllipse(drawBrush, null, this.SkeletonPointToScreen(joint.Position),
+                        JointThickness, JointThickness);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Maps a SkeletonPoint to lie within our render space and converts to Point
+        /// </summary>
+        /// <param name="skelpoint">point to map</param>
+        /// <returns>mapped point</returns>
+        private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
+        {
+            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.
+                MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
+            return new Point(depthPoint.X, depthPoint.Y);
+        }
+
+        /// <summary>
+        /// Draws a bone line between two joints
+        /// </summary>
+        /// <param name="skeleton">skeleton to draw bones from</param>
+        /// <param name="drawingContext">drawing context to draw to</param>
+        /// <param name="jointType0">joint to start drawing from</param>
+        /// <param name="jointType1">joint to end drawing at</param>
+        private void DrawBone(Skeleton skeleton, DrawingContext drawingContext, JointType jointType0, JointType jointType1)
+        {
+            Joint joint0 = skeleton.Joints[jointType0];
+            Joint joint1 = skeleton.Joints[jointType1];
+
+            // If we can't find either of these joints, exit
+            if (joint0.TrackingState == JointTrackingState.NotTracked ||
+                joint1.TrackingState == JointTrackingState.NotTracked)
+            {
+                return;
+            }
+
+            // Don't draw if both points are inferred
+            if (joint0.TrackingState == JointTrackingState.Inferred &&
+                joint1.TrackingState == JointTrackingState.Inferred)
+            {
+                return;
+            }
+
+            // We assume all drawn bones are inferred unless BOTH joints are tracked
+            Pen drawPen = this.inferredBonePen;
+            if (joint0.TrackingState == JointTrackingState.Tracked && joint1.TrackingState == JointTrackingState.Tracked)
+            {
+                drawPen = this.trackedBonePen;
+            }
+
+            drawingContext.DrawLine(drawPen, this.SkeletonPointToScreen(joint0.Position),
+                this.SkeletonPointToScreen(joint1.Position));
+        }
+
+        /// <summary>
+        /// check if one of joints colide with the wall
+        /// </summary>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool ifJointsCollideWithWall(Skeleton skeleton)
+        {
+            try
+            {
+                foreach (Joint joint in skeleton.Joints)
+                {
+                    if (this.wall[(int)this.SkeletonPointToScreen(joint.Position).Y,
+                        (int)this.SkeletonPointToScreen(joint.Position).X])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// create boolean array using bitmap - true if pixel is black, false if pixel is white
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        private bool[,] convertBitmapToBooleanArray(System.Drawing.Bitmap bitmap)
+        {
+            bool[,] boolArray = new bool[480, 640];
+
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    if (bitmap.GetPixel(j, i).R == 0 && bitmap.GetPixel(j, i).G == 0 && bitmap.GetPixel(j, i).B == 0)
+                    {
+                        boolArray[i, j] = true;
+                    }
+                    else
+                    {
+                        boolArray[i, j] = false;
+                    }
+                }
+            }
+            return boolArray;
+        }
+
+        /// <summary>
+        /// prepare and start new level
+        /// </summary>
+        private void startNewLevel()
+        {
+            counter = 5;
+            
+            this.timer = new Timer();
+            timer.Elapsed += timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Interval = 1000;
+            _wall = "wall" + level.ToString() + ".png";
+
+            System.Drawing.Bitmap img = new System.Drawing.Bitmap(_wall);
+            this.wall = convertBitmapToBooleanArray(img);
+
+            level++;
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                this.Timer.Content = "Level " + (level - 1) + " Czas: " + counter;
+            }));
+
+            timer.Start();
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (counter > 0)
+            {
+                counter--;
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    this.Timer.Content = "Level " + (level - 1) + " Czas: " + counter;
+                }));
+                timer.Start();
+            }
+            else if (counter == 0)
+            {
+                if (this.matched)
+                {
+                    MediaPlayer soundPlayer = new MediaPlayer();
+                    FileInfo good = new FileInfo("dobrze.mp3");
+
+                    soundPlayer.Open(new Uri(good.FullName));
+                    soundPlayer.Play();
+                    points++;
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.PointsLabel.Content = "Punkty: " + points;
+                    }));
+                }
+
+                else
+                {
+                    MediaPlayer soundPlayer = new MediaPlayer();
+                    FileInfo good = new FileInfo("blad.mp3");
+
+                    soundPlayer.Open(new Uri(good.FullName));
+                    soundPlayer.Play();
+                }
+
+                timer.Stop();
+                timer.Dispose();
+                timer = null;
+
+                if (level < 17)
+                {
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.startNewLevel();
+                    }));
+                }
+                else
+                {
+                    this.sensor.Stop();
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.Image.Source = null;
+                        this.closeBtn.Visibility = System.Windows.Visibility.Visible;
+                        this.restartBtn.Visibility = System.Windows.Visibility.Visible;
+                    }));
+                    MessageBox.Show("Twój wynik " + points);
+                }
+            }
+        }
+
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == closeBtn)
+            {
+                this.Close();
+            }
+            else if (sender == restartBtn)
+            {
+                this.points = 0;
+                level = 1;
+
+                this.WindowLoaded(this, null);
+            }
+        }
+    }
+}
---
+++ SplashScreen.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+
+        private MediaPlayer mediaPlayer;
+        public SplashScreen()
+        {
+            InitializeComponent();
+            
+            FileInfo mp3File = new FileInfo("1.mp3");
+            if (mp3File.Exists)
+            {
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.MediaEnded += mediaPlayer_MediaEnded;
+                mediaPlayer.Open(new Uri(mp3File.FullName));
+                mediaPlayer.Play();
+            }
+
+        }
+
+        void mediaPlayer_MediaEnded(object sender, EventArgs e)
+        {
+            mediaPlayer.Close();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Close();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
